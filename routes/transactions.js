682ace3ec2b6221dfec31f55080ec1aecc579d03
sankalp0412 <dhuparsankalp@gmail.com>
2023-06-29 18:37:19 +0530

    Added Base URL

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
+const BASE_URL = process.env.BASE_URL;
 
 router
-    .route('/')
+    .route(`${BASE_URL}/`)
     .get(getTransactions)
     .post(addTransaction);
 
 router
-    .route('/:id')
+    .route(`${BASE_URL}/:id`)
     .delete(deleteTransaction);
 
 module.exports = router;
